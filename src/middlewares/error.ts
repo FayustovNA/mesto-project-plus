686e309494dd60ccde9cc7c1472bb99b76cf4004
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-07-09 08:23:45 +0300

    after 3d review

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,16 +1,17 @@
 import {
   Response,
   Request,
+  NextFunction,
 } from 'express';
 import { Error } from '../types/type';
 import ApiError from '../errors/api-err';
 
 const { SERVER_ERROR } = require('../errors/config-err');
 
-export default (err: Error, req: Request, res: Response) => {
+export default (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
   }
-  return res.status(SERVER_ERROR).json({ message: 'Server error' });
+  return next(res.status(SERVER_ERROR).json({ message: 'Server error' }));
 };
